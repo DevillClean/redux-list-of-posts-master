e7df838c5db7a0afe771c91b3892f38eb52bac01
DevillClean <sudziro62@gmail.com>
2023-11-25 01:05:59 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+
+import { store } from './app/store';
+import { App } from './App';
+import { UsersProvider } from './components/UsersContext';
+
+const Root = () => (
+  <Provider store={store}>
+    {/* Remove UsersProvider when you move users to Redux store */}
+    <UsersProvider>
+      <Router>
+        <App />
+      </Router>
+    </UsersProvider>
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
